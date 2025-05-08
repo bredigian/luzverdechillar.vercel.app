47eed31c7e7f0a2e79bc5ae9dea963d5efbf15c2
Gianluca Bredice <bredicegian@gmail.com>
2025-05-08 11:02:33 -0300

    fix: add extra styles to events was past on schedule component

--- src/components/schedule.tsx
+++ src/components/schedule.tsx
@@ -3,6 +3,7 @@
 import { DateTime } from "luxon"
 import { EstimateProps } from "@/types/estimates.types"
 import FullCalendar from "@fullcalendar/react"
+import { cn } from "@/lib/utils"
 import dayGridPlugin from "@fullcalendar/daygrid"
 
 interface Props {
@@ -19,23 +20,31 @@ export default function Schedule({ data }: Props) {
       displayEventEnd
       nowIndicator={false}
       initialView="dayGridMonth"
-      events={data.map((estimate, index) => ({
-        start: DateTime.fromJSDate(new Date(estimate.from)).toJSDate(),
-        end: DateTime.fromJSDate(new Date(estimate.to))
-          .plus({ days: 1 })
-          .toJSDate(),
-        title: `${estimate.person.firstName} ${
-          estimate.person.lastName
-        } ${Intl.NumberFormat("es-AR", {
-          style: "currency",
-          currency: "ARS",
-          minimumFractionDigits: 2,
-        }).format(estimate.totalCost)}`,
-        allDay: true,
-        id: estimate._id,
-        color: COLORS[index],
-        textColor: TEXT_COLORS[index],
-      }))}
+      events={data.map((estimate, index) => {
+        const startDate = DateTime.fromJSDate(new Date(estimate.from))
+        const endDate = DateTime.fromJSDate(new Date(estimate.to)).plus({
+          days: 1,
+        })
+
+        const eventPast = DateTime.now().toMillis() > endDate.toMillis()
+
+        return {
+          start: startDate.toJSDate(),
+          end: endDate.toJSDate(),
+          title: `${estimate.person.firstName} ${
+            estimate.person.lastName
+          } ${Intl.NumberFormat("es-AR", {
+            style: "currency",
+            currency: "ARS",
+            minimumFractionDigits: 2,
+          }).format(estimate.totalCost)}`,
+          allDay: true,
+          id: estimate._id,
+          color: COLORS[index],
+          textColor: TEXT_COLORS[index],
+          className: cn(eventPast ? "!line-through !opacity-50" : ""),
+        }
+      })}
       eventContent={EventContent}
       eventClassNames={"mb-1 truncate px-2 py-1"}
       height={"90dvh"}
