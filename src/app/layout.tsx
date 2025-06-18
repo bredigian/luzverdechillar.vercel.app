af6d2f121162e275650037d94623248d55ed484a
Gianluca Bredice <bredicegian@gmail.com>
2025-06-18 15:07:53 -0300

    _feat: configure PWA support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,16 +11,35 @@ const geistSans = Geist({
   subsets: ["latin"],
 })
 
-export const viewport: Viewport = {
-  width: "device-width",
-  userScalable: false,
-  initialScale: 1,
-  maximumScale: 1,
-}
+const APP_NAME = "Luz Verde Chillar"
+const APP_DEFAULT_TITLE = "Luz Verde Chillar"
+const APP_TITLE_TEMPLATE = "%s | Luz Verde Chillar"
+const APP_DESCRIPTION = "Generador de presupuestos."
 
 export const metadata: Metadata = {
-  title: "Luz Verde Chillar | Generador de presupuestos",
-  description: "Generador de presupuestos",
+  applicationName: APP_NAME,
+  description: APP_DESCRIPTION,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+  },
+  formatDetection: {
+    telephone: false,
+  },
+}
+
+export const viewport: Viewport = {
+  userScalable: false,
+  themeColor: [
+    { media: "(prefers-color-scheme: dark)", color: "#000000" },
+    { media: "(prefers-color-scheme: light)", color: "#ffffff" },
+  ],
 }
 
 export default function RootLayout({
