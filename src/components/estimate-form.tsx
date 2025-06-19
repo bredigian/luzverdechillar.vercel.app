36419183363b615e7494ce651a8b95bc61cc0b0f
Gianluca Bredice <bredicegian@gmail.com>
2025-06-19 15:38:22 -0300

    fix: change extra services ui in estimate form

--- src/components/estimate-form.tsx
+++ src/components/estimate-form.tsx
@@ -348,7 +348,7 @@ export function EstimateForm({ categories, defaultData }: Props) {
       />
       <div className="flex flex-col gap-4">
         <aside className="flex items-center gap-2 self-end">
-          <Label>Descuento</Label>
+          <Label>Descuento sobre Electro Instalador</Label>
           <div className="relative flex items-center max-w-16">
             <Percent className="absolute size-4 end-0 mr-3" />
             <Input
@@ -362,7 +362,7 @@ export function EstimateForm({ categories, defaultData }: Props) {
           </div>
         </aside>
         <aside className="flex items-center justify-between gap-4">
-          <Label className="grow">Servicios (electro instalador)</Label>
+          <Label className="grow">Servicios (Electro Instalador)</Label>
           {servicesError && <Warning message="Incompleto" />}
           <Button
             variant={"destructive"}
@@ -494,7 +494,7 @@ export function EstimateForm({ categories, defaultData }: Props) {
             Extra
           </Button>
         </aside>
-        <ul className="flex flex-col gap-4 md:max-h-[140px] overflow-y-auto">
+        <ul className="flex flex-col gap-4 md:max-h-48 overflow-y-auto">
           {extraSelectedServices.length === 0 ? (
             <li className="font-thin text-sm w-full flex items-center gap-2">
               <span className="h-0.5 w-full bg-muted"></span>
@@ -504,123 +504,126 @@ export function EstimateForm({ categories, defaultData }: Props) {
           ) : (
             extraSelectedServices.map((extraService, index) => (
               <li
-                className="flex items-center justify-between gap-4"
+                className="flex flex-col gap-4 w-full"
                 key={`extra_services_${index}`}
               >
-                <Input
-                  type="text"
-                  placeholder="Ej: Combustible"
-                  className="text-sm min-w-0 w-[17rem] grow"
-                  value={extraService.description}
-                  onChange={({ target }) =>
-                    extraHandleChangeSelectValue(
-                      {
-                        _id: `extra_${index}`,
-                        id: `extra_${index}`,
-                        description: target.value,
-                        category: extraService.category || "Extra",
-                        type: "-",
-                        cost: extraService.cost,
-                        originalCost: extraService.originalCost,
-                      },
-                      index
-                    )
-                  }
-                  disabled={status === "success"}
-                />
-                <div className="relative flex items-center">
-                  <span
-                    className={cn(
-                      "absolute pl-3 pt-0.5",
-                      !extraService.id || status === "success"
-                        ? "opacity-50"
-                        : "opacity-100"
-                    )}
+                <div className="flex items-center gap-4 w-full">
+                  <Input
+                    type="text"
+                    placeholder="Ej: Combustible"
+                    className="text-sm min-w-0 w-[17rem] grow"
+                    value={extraService.description}
+                    onChange={({ target }) =>
+                      extraHandleChangeSelectValue(
+                        {
+                          _id: `extra_${index}`,
+                          id: `extra_${index}`,
+                          description: target.value,
+                          category: extraService.category || "Extra",
+                          type: "-",
+                          cost: extraService.cost,
+                          originalCost: extraService.originalCost,
+                        },
+                        index
+                      )
+                    }
+                    disabled={status === "success"}
+                  />
+                  <Button
+                    size={"icon"}
+                    type="button"
+                    onClick={() => extraDeleteService(index)}
+                    disabled={status === "success"}
                   >
-                    $
-                  </span>
-                  <CurrencyInput
-                    disabled={!extraService.id || status === "success"}
-                    defaultValue={extraService?.cost || 0}
-                    decimalsLimit={2}
-                    allowDecimals={false}
-                    className={cn(
-                      "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-                      "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
-                      "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
-                      "pl-7"
-                    )}
-                    value={extraService.cost ?? 0}
-                    onValueChange={(value, name, values) => {
-                      extraHandleChangeServiceCost(
-                        Number(values?.float),
+                    <Trash />
+                  </Button>
+                </div>
+                <div className="flex items-center gap-4 w-full">
+                  <div className="relative flex items-center">
+                    <span
+                      className={cn(
+                        "absolute pl-3 pt-0.5",
+                        !extraService.id || status === "success"
+                          ? "opacity-50"
+                          : "opacity-100"
+                      )}
+                    >
+                      $
+                    </span>
+                    <CurrencyInput
+                      disabled={!extraService.id || status === "success"}
+                      defaultValue={extraService?.cost || 0}
+                      decimalsLimit={2}
+                      allowDecimals={false}
+                      className={cn(
+                        "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+                        "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
+                        "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+                        "pl-7"
+                      )}
+                      value={extraService.cost ?? 0}
+                      onValueChange={(value, name, values) => {
+                        extraHandleChangeServiceCost(
+                          Number(values?.float),
+                          extraService.id as string
+                        )
+                      }}
+                      decimalSeparator=","
+                      groupSeparator="."
+                    />
+                  </div>
+                  <Input
+                    type="number"
+                    min={1}
+                    className="text-center text-sm max-w-14"
+                    placeholder={extraService.quantity?.toString() ?? "1"}
+                    onChange={({ target }) =>
+                      extraHandleChangeServiceQuantity(
+                        Number(target.value) < 1 ? 1 : Number(target.value),
                         extraService.id as string
                       )
-                    }}
-                    decimalSeparator=","
-                    groupSeparator="."
+                    }
+                    disabled={!extraService.description || status === "success"}
                   />
+                  <Select
+                    onValueChange={(value) => {
+                      extraHandleChangeSelectValue(
+                        {
+                          _id: `extra_${index}`,
+                          id: `extra_${index}`,
+                          description: extraService.description,
+                          category: value,
+                          type: "-",
+                          cost: extraService.cost,
+                          originalCost: extraService.originalCost,
+                        },
+                        index
+                      )
+                    }}
+                    defaultValue={extraService.category || "Material"}
+                  >
+                    <SelectTrigger className="grow cursor-pointer">
+                      <SelectValue placeholder="Material" />
+                    </SelectTrigger>
+                    <SelectContent align="end">
+                      <SelectGroup>
+                        <SelectLabel>Tipo</SelectLabel>
+                        <SelectItem
+                          value="Material"
+                          className="hover:bg-accent cursor-pointer"
+                        >
+                          Material
+                        </SelectItem>
+                        <SelectItem
+                          value="Extra"
+                          className="hover:bg-accent cursor-pointer"
+                        >
+                          Extra
+                        </SelectItem>
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
                 </div>
-                <Input
-                  type="number"
-                  min={1}
-                  className="text-center text-sm max-w-10"
-                  placeholder={extraService.quantity?.toString() ?? "1"}
-                  onChange={({ target }) =>
-                    extraHandleChangeServiceQuantity(
-                      Number(target.value) < 1 ? 1 : Number(target.value),
-                      extraService.id as string
-                    )
-                  }
-                  disabled={!extraService.description || status === "success"}
-                />
-                <Select
-                  onValueChange={(value) => {
-                    extraHandleChangeSelectValue(
-                      {
-                        _id: `extra_${index}`,
-                        id: `extra_${index}`,
-                        description: extraService.description,
-                        category: value === "M" ? "Material" : "Extra",
-                        type: "-",
-                        cost: extraService.cost,
-                        originalCost: extraService.originalCost,
-                      },
-                      index
-                    )
-                  }}
-                  defaultValue={extraService.category.charAt(0) || "E"}
-                >
-                  <SelectTrigger className="max-w-16 cursor-pointer">
-                    <SelectValue placeholder="E" />
-                  </SelectTrigger>
-                  <SelectContent align="end">
-                    <SelectGroup>
-                      <SelectLabel>Tipo</SelectLabel>
-                      <SelectItem
-                        value="M"
-                        className="hover:bg-accent cursor-pointer"
-                      >
-                        <strong>M</strong> (Material)
-                      </SelectItem>
-                      <SelectItem
-                        value="E"
-                        className="hover:bg-accent cursor-pointer"
-                      >
-                        <strong>E</strong> (Extra)
-                      </SelectItem>
-                    </SelectGroup>
-                  </SelectContent>
-                </Select>
-
-                <Button
-                  size={"icon"}
-                  type="button"
-                  onClick={() => extraDeleteService(index)}
-                  disabled={status === "success"}
-                >
-                  <Trash />
-                </Button>
               </li>
             ))
           )}
